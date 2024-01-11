35166267f842913f28474a7642e5cbfa752643b5
FabiSahne <65239050+FabiSahne@users.noreply.github.com>
2024-01-11 15:34:07 +0100

    Update build-static.sh

--- aufgabe6/build-static.sh
+++ aufgabe6/build-static.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
 g++ -c -Wall -Wextra -Werror -pedantic lib/benotung.cpp lib/fachnote.cpp lib/fachnoten_liste.cpp
-ar rvs libaufgabe6.a benotung.o fachnote.o fachnoten_liste.o
+ar rcs libaufgabe6.a benotung.o fachnote.o fachnoten_liste.o
